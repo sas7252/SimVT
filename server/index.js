4fa22fec5e1cfc1738021057505c872975bb88b0
Samuel <11140199+SamyLS@users.noreply.github.com>
2020-05-05 18:05:53 +0200

    Added server controller
    Server module acts as controller for the 2 service modules.

---
+++ server/index.js
@@ -0,0 +1,42 @@
+/**
+ * Server
+ */
+const contentSvc = require('./services/contentService');
+const signalSvc = require('./services/signalService');
+
+module.exports = {
+    isRunning,
+    startWithDelegate,
+    stop,
+    sendSignal
+}
+
+let running = false;
+
+function isRunning() {
+    return running;
+}
+
+function startWithDelegate(delegate) {
+    if (!running) {
+        contentSvc.start(8080, './server/data/clients');
+        signalSvc.start(8081, delegate.handleIncommingSignal);
+        signalSvc.useInboundFilter(delegate.filterSignalToServer);
+        running = true;
+    }
+}
+
+function stop() {
+    if (running) {
+        console.log("Stopping server");
+        contentSvc.stop();
+        signalSvc.stop();
+        running = false;
+    }
+}
+
+function sendSignal(signal) {
+    if (running) {
+        signalSvc.broadcastSignal(signal);
+    }
+}
