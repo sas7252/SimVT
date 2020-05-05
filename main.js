feaa4b623f13db9215c9ff5616607a8f8c61a5a6
Samuel <11140199+SamyLS@users.noreply.github.com>
2020-05-05 17:58:48 +0200

    Added application core
    Added nodeJS project, electron and future dependencies like express web server and websockets module.
    
    Implemented main.js (application core)

---
+++ main.js
@@ -0,0 +1,27 @@
+
+    const {app} = require('electron');
+    const appInfo = require('./app.properties.json');
+    
+    //Overwrite default log-function
+    console.logCopy = console.log.bind(console);
+    
+    console.log = function(data)
+    {
+        var currentDate = '[' + new Date().toLocaleString() + '] ';
+        this.logCopy(currentDate, data);
+    };
+    
+    let didLaunch = false;
+    let ready = true
+    
+    function launch() {
+        if (!didLaunch && ready) {
+            ready = false;
+            console.log("Launching " + appInfo.productName + " " + appInfo.productVersionString);
+            //appController.initWithProperties(appInfo);
+            didLaunch = true;
+            ready = true;
+        }
+    }
+    
+    app.whenReady().then(launch);
