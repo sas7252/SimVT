cb8fdd0bdb70b449008b71d81f8925782c9af4bf
Samuel Schreiber <sa7252@thi.de>
2020-05-12 01:45:05 +0200

    Finalized DriveMe UI
    Updated client module (app.js) to work with the latest webui.js version (2).
    Implemented
    - beforeLeavingPanel
    - beforeShowingPanel
    (those replace the old nextPanel method which was moved to webui.js)
    - initClientModule
    (replaced old applyClinetCustomizations)
    
    Added fake/real dummy application logic, like updating shown tos/toa in "realtime"
    - startBookingEstimationsUpdater
    - stopBookingEstimationsUpdater
    - updateBookingEstimations

--- server/data/clients/app/resources/javascript/app.js
+++ server/data/clients/app/resources/javascript/app.js
@@ -1,38 +1,90 @@
-//Overwrite custom nextPanel
-app.nextPanel = function() {
-    switch (this.cPanel.selector) {
+app.bookedTrip = {
+    toa: null,
+    tos: null,
+    date: this.getFormattedDate,
+    price: '20 €',
+    available: false
+}
+
+app.beforeShowingPanel = function(nextPanelId) {
+    switch (nextPanelId) {
         case 'pid_mapp_s3_launch':
-            //No default action. This is the loading panel
             break;
         case 'pid_mapp_s3_main':
-            this.showPanel('pid_mapp_s3_booking_new');
+            if (this.bookedTrip.available) this.addBookedTripToMainPanel();
             break;
         case 'pid_mapp_s3_booking_new':
-            this.showPanel('pid_mapp_s3_booking_confirm');
+            $('input#set_bookedTrip_time').attr('value', this.getFormattedTime());
             break;
         case 'pid_mapp_s3_booking_confirm':
-            this.showPanel('pid_mapp_s3_booking_confirmation');
+            this.startBookingEstimationsUpdater();
             break;
         case 'pid_mapp_s3_booking_confirmation':
-            this.addBookedTrip();
-            this.showPanel('pid_mapp_s3_main');
+            $('.ui_bookedTrip_time').html(this.bookedTrip.tos); //Update takeover time from confirm screen into this confirmation
             break;
-        case 'webui_error':
-            this.reload();
         default:
-            this.showPanel('webui_error');
+            this.showPanelWithoutNotifications('webui_error');
             throw "Illegal panel id. Can not continiue (!)";
     }
 }
 
-app.addBookedTrip = function () {
-    $('.ui_bookedTrip_date').html($('input#set_bookedTrip_date').value);
-    $('.ui_bookedTrip_time').html($('input#set_bookedTrip_time').value);
+app.beforeLeavingPanel = function(leavingPanelId) {
+    switch (leavingPanelId) {
+        case 'pid_mapp_s3_launch':
+        case 'pid_mapp_s3_main':
+        case 'pid_mapp_s3_booking_new':
+            break;
+        case 'pid_mapp_s3_booking_confirm':
+            this.stopBookingEstimationsUpdater();
+            break;
+        case 'pid_mapp_s3_booking_confirmation':
+            //$('.ui_bookedTrip_time').html(this.bookedTrip_tos); //Update takeover time from confirm screen into this confirmation
+            break;
+        default:
+            this.showPanelWithoutNotifications('webui_error');
+            throw "Illegal panel id. Can not continiue (!)";
+    }
+}
+
+app.startBookingEstimationsUpdater = function() {
+    if (!this.bookingEstimationsUpdater) {
+        console.log("Starting bookingEstimationsUpdater");
+        this.updateBookingEstimations();
+        this.bookingEstimationsUpdater = setInterval(function() {
+            app.updateBookingEstimations();
+        }, 5000);
+    }
+}
+
+app.updateBookingEstimations = function() {
+    bt = this.bookedTrip;
+    bt.tos = this.getFormattedTime(0); //Time of Start
+    bt.toa = this.getFormattedTime(+20); //Time of Arrival (= TOS+20 Minuten)
+    console.log('Updating booking estimations: TOS@' + bt.tos + " --> TOA@" + bt.toa);
+    $('.ui_bookingEstimation_tos').html(bt.tos);
+    $('.ui_bookingEstimation_toa').html(bt.toa);
+    app.bookedTrip_tos = this.getFormattedTime(+1); //This will be shown in confirmation screen as takeover time
+}
+
+app.stopBookingEstimationsUpdater = function() {
+    if (this.bookingEstimationsUpdater) {
+        console.log("Stopping bookingEstimationsUpdater");
+        clearInterval(this.bookingEstimationsUpdater);
+        this.bookingEstimationsUpdater = false;
+    }
+}
+
+app.addBookedTripToMainPanel = function() {
+    $('.ui_bookedTrip_date').html(this.bookedTrip.date);
+    $('.ui_bookedTrip_time').html(this.bookedTrip.tos);
     $('.ui_bookedTrip_cost').html("20 €*");
     $('#ui_bookedTrip').show();
+    console.info("Added booked trip to main panel")
 }
 
-app.applyClientCustomizations = function() {
+app.initClientModule = function() {
     $('#ui_bookedTrip').hide();
+    this.bookingEstimationsUpdater = false;
+    this.setHomePanel('pid_mapp_s3_main');
+    console.info("Client module initialized");
 }
-
