6fdf82111d03ecb5a232bb395757b42cf423f14b
Samuel Schreiber <git@samyls.net>
2020-05-21 00:15:53 +0200

    fixed websocket client and addet to carui webclient

--- server/data/clients/shared/assets/javascript/websocket.js
+++ server/data/clients/shared/assets/javascript/websocket.js
@@ -7,32 +7,34 @@ __f = function() {
 }
 
 let ws = {
-    __connected: false
+    __connected: false,
+    __delegateFunction: function () {}
 }
 
 ws.__onConnect = function(evt) {
     this.__connected = true;
-    console.info('Sucessfully connected to ' + this.__serverAddress);
+    console.info('WSCLIENT: CONNECTION ESTABLISHED');
 }
 
 ws.__onClose = function(evt) {
-    console.warn('LOST CONNECTION TO SERVER');
-    alert("Server is not reponding");
+    console.warn('WSCLIENT: LOST CONNECTION TO SIGNAL SERVICE');
+    console.info('WSCLIENT: We should propably try again... (Add Implementation?)');
+    alert("SignalServer is not reponding");
     this.__connected = false;
 }
 
 ws.__onMessage = function(evt) {
-    this.__delegateFunction(evt.data);
+    ws.__delegateFunction(evt.data);
 }
 
 ws.__onError = function(evt) {
     console.error('WSClient Error: ' + evt);
 }
 
-ws.connect = function(serverAddress, delegateFunction) {
+ws.connect = function(serverAddress, delegate) {
     if (!this.__connected) {
-        this.__serverAddress = serverAddress;
-        this.__delegate = delegateFunction;
+        ws.__delegateFunction = delegate;
+        console.log("WSCLIENT: Connecting to " + serverAddress);
         this.__ws = new WebSocket(serverAddress);
         this.__ws.onopen = this.__onConnect;
         this.__ws.onclose = this.__onClose;
@@ -46,6 +48,6 @@ ws.disconnect = function() {
 }
 
 ws.sendSignal = function(signal) {
-    console.log('Sending signal [' + signal + '] to server');
-    this._ws.send(signal);
+    console.log('WSCLIENT: Sending signal [' + signal + '] to server');
+    ws.__ws.send(signal);
 }
