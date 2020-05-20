33fe1d9c529d02e581b4c8bccd9300a0ace63dbf
Samuel Schreiber <sa7252@thi.de>
2020-05-20 16:14:18 +0200

    WIP build actual local frontend UI

--- appController.js
+++ appController.js
@@ -22,6 +22,39 @@ module.exports = {
     filterSignalToClient
 }
 
+global.appinfo = {
+    title: 'NULL'
+}
+
+global.signals = {
+    out: {
+        reload: 'op2car_reload',
+        rtOffer: 'op2car_rtOFFER',
+        rtOnline: 'op2car_rtONLINE',
+        rtRelease: 'op2car_rtRELEASE',
+        rtOffline: 'op2car_rtOFFLINE'
+    },
+    in: {
+        rtOfferAccept: 'car2op_rtOFFER-accept',
+        rtOfferDecline: 'car2op_rtOFFER-decline',
+        rtReleaseConfirmNo: 'car2op_rtRELEASE-confirm:no',
+        rtReleaseConfirmYes: 'car2op_rtRELEASE-confirm:yes',
+        rtReleaseReq: 'car2op_rtRELEASE-request',
+        rtBooked: 'app2op_rtTripBooked'
+    },
+    app: {
+        exit: 'appCM_exit',
+        hide: 'appCM_hide',
+        notifyCfgUpdated: 'appCM_updateCfg'
+    }
+}
+
+global.simCfg = {
+    sNr: 2,
+    iavNr: 1,
+    dNr: 1
+}
+
 //let frontend = null;
 
 let quitNow = false;
@@ -54,10 +87,17 @@ function activateWindow() {
     }
 }
 
+function hideWindow() {
+    if (ready) {
+        window = frontend.getWindow();
+        window.hide();
+    }
+}
+
 function openBrowserWindowDeveloperTools() {
     if (ready) {
         window = frontend.getWindow();
-        window.webContents.openDevTools({mode:'detach'});
+        window.webContents.openDevTools({ mode: 'detach' });
         console.log('opened developer tools for local window');
     }
 }
@@ -74,59 +114,133 @@ function quitApplication() {
 function handleIncommingSignal(signal, sender) {
     if (filterSignalToServer(signal)) {
         addMessageToWindowConsole("<--IN- [" + signal + "] from client " + sender);
+        switch (signal) {
+            case signals.in.rtOfferAccept:
+                vc.showElement(vc.banners.rtOfferAccepted);
+                break;
+            case signals.in.rtOfferDecline:
+                vc.showElement(vc.banners.rtOfferDeclined, 5);
+                break;
+            case singals.in.rtReleaseConfirmNo:
+                vc.showElement(vc.banners.rtReleaseReqDeclined, 5);
+                break;
+            case signals.in.rtReleaseConfirmYes:
+                vc.showElement(vc.banners.rtReleaseReqAccepted);
+                break;
+            case signals.in.rtReleaseReq:
+                vc.showElement(vc.banners.rtReleaseRequestByUser);
+                break;
+            case signals.in.rtBooked:
+                if (global.simCfg.sNr == 3) {
+                    vc.showElement(vc.banners.rtBooked)
+                } else {
+                    console.log("Recieved invalid signal [" + signals.in.rtBooked + "] (only available in S3)")
+                }
+                break;
+        }
     } else if (signal == "CLIENT_CONNECTED") {
-            addMessageToWindowConsole("<----> CONNECTED " + sender, "blue");
+        addMessageToWindowConsole("<----> CONNECTED " + sender, "blue");
     } else if (signal == "CLIENT_DISCONNECTED") {
         addMessageToWindowConsole("<-X X-> DISCONNECTED " + sender, "red");
     }
 }
 
-function sendCommandSignalToRemoteClients(signal) {
-    if (filterSignalToClient(signal)) server.sendSignal(signal)
+function sendCmdSignalToClients(signal) {
+    if (filterSignalToClient(clientCommand)) {
+        console.log("Command signal from admin: " + clientCommand + " Sending to clients now!");
+        server.sendSignal(clientCommand);
+        addMessageToWindowConsole("-OUT-> [" + clientCommand + "] to Client", "green");
+    } else {
+        if (filterSignalToServer(arg)) {
+            addMessageToWindowConsole("(!) [" + clientCommand + "] is a client->server signal and can not be send from the server!", "orange");
+        } else {
+            addMessageToWindowConsole("(!) [" + clientCommand + "] is not a valid signal", "orange");
+        }
+    }
 }
 
 function addMessageToWindowConsole(message, color) {
-    var window = frontend.getWindow();
-    window.webContents.executeJavaScript("addMessageToWindowConsole('" + message + "','" + color + "');");
+    vc.execute('addToConsole', message, color);
+}
+
+function valueExistsInObject(value, object) {
+    Object.keys(object).forEach(function(key) {
+        if (object[key] == value) {
+            return true;
+        }
+    });
+    return false;
+}
+
+function validSignal(direction, signal) {
+    if (direction == 'in') return valueExistsInObject(global.signals.in, signal);
+    if (direction == 'out') return valueExistsInObject(global.signals.out, signal);
+    return false;
 }
 
 function filterSignalToServer(signal) {
-    switch (signal) {
-        case "CAR_RT_ACCEPT_BY_USER":
-        case "CAR_RT_CANCEL_BY_USER":
-        case "CAR_RT_ONLINE":
-        case "CAR_RT_OFFLINE":
-        case "CAR_RT_STOP":
-        case "CAR_RT_REQUEST_BY_USER":
-        case "APP_RT_REQUEST_BY_USER":
-            return true;
-        default:
-            return false;
-    }
+    return validSignal('in', signal);
 }
 
 function filterSignalToClient(signal) {
-    switch (signal) {
-        case "OPERATOR_ACCEPT_APP_RT_REQUEST":
-        case "OPERATOR_ACCEPT_CAR_RT_REQUEST":
-        case "OPERATOR_OFFER_RT_IN_VEHICLE":
-            return true;
-        default:
-            return false;
-    }
+    return validSignal('out', signal);
 }
 
-ipcMain.on('asynchronous-message', (event, arg) => {
+ipcMain.on('clientCommand', (event, clientCommand) => {
     //console.log("Recieved command from renderer: " + arg);
-    if (filterSignalToClient(arg)) {
-        console.log("Command signal from admin: " + arg + " Sending to clients now!");
-        server.sendSignal(arg);
-        addMessageToWindowConsole("-OUT-> ["+ arg + "] to Client", "green");
-    } else {
-        if (filterSignalToServer(arg)) {
-            addMessageToWindowConsole("(!) [" + arg + "] is a client->server signal and can not be send from the server!", "orange");
-        } else {
-            addMessageToWindowConsole("(!) [" + arg + "] is not a valid signal", "orange");
+    sendCmdSignalToClients(clientCommand);
+})
+
+ipcMain.on('appCommand', (event, appCommand) => {
+    if (appCommandValid(appCommand)) {
+        console.log('Executing appCommand [' + appCommand + '] from renderer process');
+        switch (appCommand) {
+            case signals.app.exit:
+                quitApplication();
+                break;
+            case signals.app.hide:
+                hideWindow();
+                break;
+            case signals.app.notifyCfgUpdated:
+                sendCmdSignalToClients(signals.out.reload);
+                break;
+            default:
+                throw "Invalid appCommand: " + appCommand;
         }
     }
-})
+});
+
+var vc = {
+
+    //Add banners for reference
+    banners: {
+        rtOfferDeclined = '.ui_banner_rtoffer_declined',
+        rtOfferAccepted = '.ui_banner_rtoffer_accepted',
+        rtReleaseRequestByUser = '.ui_banner_rtrelease_reqByUser',
+        rtReleaseReqDeclined = '.ui_banner_rtrelease_reqDeclined',
+        rtReleaseReqAccepted = '.ui_banner_rtrelease_reqAccepted',
+        rtReleaseAction = '.ui_banner_rtrelease_actNow',
+        rtBooked = '.ui_banner_rtbooked'
+    },
+
+    //Add execute function
+    execute: function(method, arg1, arg2 = false) {
+        //build the actual function call
+        var rfCall; // Remote function call
+        rfCall = "vc." + method + "('" + arg1 + "'";
+        if (arg2) {
+            rfCall += ","
+            if (typeof arg2 == 'number') {
+                rfCall += arg2;
+            } else if (typeof arg2 == 'string') {
+                rfCall += ("'" + arg2 + "'");
+            } else {
+                rfCall += "''";
+            }
+        }
+        rfCall = rfCall + ");";
+
+        //get window and execute statement
+        frontend.getWindow().webContents.executeJavaScript(rfCall);
+    }
+}
--- frontend/resources/views/javascript/app.js
+++ frontend/resources/views/javascript/app.js
@@ -1,38 +1,176 @@
 const textviewConsole = document.getElementById('uiTextView_console');
 
 const { ipcRenderer } = require('electron');
+const { remote } = require('electron').remote;
+
+__f = function() {
+    throw "implementation missing";
+}
+
+//ViewController "Class"
+vc = viewController = {
+
+    //Initializers
+    init: __f,
+
+    //Variables & Objects
+    _appinfo: null,
+    _signals: null,
+
+    _outlets: {
+        console: null,
+        txtfCmd: null,
+        dropdown_sNr: null,
+        dropdown_iavNr: null,
+        dropdown_dNr: null,
+        btn_runCmd: null
+    },
+
+    _actions: {
+        exitApp: 'vca_quit',
+        hideApp: 'vca_hide',
+        sendRtOffer: 'vca_sendRtOffer',
+        sendTbOffer: 'vca_sendTbOffer',
+        sendRTOnline: 'vca_sendRtOnline',
+        sendRtOffline: 'vca_sendRtOffline',
+        publishConfiguration: 'vca_publishCfg',
+        runCustomCommand: 'vca_runCmd'
+    },
+
+    //Methods
+    triggerAction: __f,
+    executeAppCommand: __f,
+    sendClientCommand: __f,
+    readCurrentSimCfg: __f,
+    writeCurrentSimCfg: __f,
+    showElement: __f,
+    hideElement: __f,
+    addToConsole: __f,
+
+}
+
+vc.init = function() {
+
+    this._appinfo = remote.getGlobal('appInfo');
+    this._signals = remote.getGlobal('signals');
 
 
-/*
-function addMessageToWindowConsole(message, color) {
+
+    //Wire up UI Outlets
+    this._outlets.console = $('#ui_consoleView');
+    this._outlets.txtfCmd = $('#ui_textfield_commandInput');
+    this._outlets.dropdown_sNr = $('#ui_dropdown_sNr');
+    this._outlets.dropdown_iavNr = $('#ui_dropdown_sNr');
+    this._outlets.dropdown_dNr = $('#ui_dropdown_sNr');
+    this._outlets.btn_runCmd = $('.ui_btn_runCmd');
+
+    //Wire up the buttons
+    $('.ui_btn_exitApp').click(vc.triggerAction(vc._actions.exitApp));
+    $('.ui_btn_hideApp').click(vc.triggerAction(vc._actions.hideApp));
+    $('.ui_btn_sendRtOffer').click(vc.triggerAction(vc._actions.sendRtOffer));
+    $('.ui_btn_sendTbOffer').click(vc.triggerAction(vc._actions.sendTbOffer));
+    $('.ui_btn_sendRtOnline').click(vc.triggerAction(vc._actions.sendRTOnline));
+    $('.ui_btn_sendRtOffline').click(vc.triggerAction(vc._actions.sendRtOffline));
+    $('.ui_btn_resetCfg').click(vc.triggerAction(vc._actions.publishConfiguration));
+    $('.ui_btn_applyCfg').click(vc.triggerAction(vc._actions.publishConfiguration));
+    this._outlets.btn_runCmd.click(vc.triggerAction(vc._actions.runCustomCommand));
+
+    //Add eevent for: enter key on textfield -> send now
+    this._outlets.txtfCmd.keyup(function(event) {
+        // Number 13 is the "Enter" key on the keyboard
+        if (event.keyCode === 13) {
+            event.preventDefault(); // Cancel the default action, if needed
+            vc._outlets.btn_runCmd.click(); // Trigger the button element with a click
+        }
+    });
+
+    this.readCurrentSimCfg();
+}
+
+vc.executeAppCommand = function(appCommand) {
+    console.log('Calling main process to (executeAppCommand: [' + appCommand + '])');
+    ipcRenderer.send('appCommand', appCommand);
+}
+
+vc.sendClientCommand = function(clientCommand) {
+    console.log('Calling main process to (sendClientCommand: [' + clientCommand + '])');
+    ipcRenderer.send('clientCommand', clientCommand);
+}
+
+vc.triggerAction = function(actionId) {
+    switch (actionId) {
+        case this._actions.exitApp:
+            this.executeAppCommand(this._signals.app.exit);
+            break;
+        case this._actions.hideApp:
+            this.executeAppCommand(this._signals.app.hide);
+            break;
+        case this._actions.publishConfiguration:
+            this.writeCurrentSimCfg();
+            this.executeAppCommand(this._signals.app.notifyCfgUpdated);
+            break;
+        case this._actions.sendRtOffer:
+            this.sendClientCommand(this._signals.out.rtOffer);
+            break;
+        case this._actions.sendTbOffer:
+            this.sendClientCommand(this._signals.out.tbOffer);
+            break;
+        case this._actions.sendRTOnline:
+            this.sendClientCommand(this._signals.out.rtOnline);
+            break;
+        case this._actions.sendRtOffline:
+            this.sendClientCommand(this._signals.out.rtOffline);
+            break;
+        case this._actions.runCustomCommand:
+            //SUGGESTION: Should maybe add something like disable the button until this action is complete?
+            var customCommand = vc._outlets.txtfCmd.val();
+            vc._outlets.txtfCmd.val('');
+            this.sendClientCommand(customCommand);
+            break;
+        default:
+            console.error('Unkown actionId: ' + actionId);
+    }
+}
+
+vc.readCurrentSimCfg = function() {
+    var simCfg = remote.getGlobal('simCfg');
+
+    this._outlets.dropdown_sNr.val(simCfg.sNr);
+    this._outlets.dropdown_iavNr.val(simCfg.iavNr);
+    this._outlets.dropdown_dNr.val(simCfg.dNr);
+
+    //Adapt view
+    var sNr = remote.getGlobal('simCfg').sNr;
+    var iavNr = remote.getClobal()
+    $('.ui_onlyS2').hide();
+    $('.ui_onlyS3').hide();
+    $('.ui_onlyS' + sNr).show();
+}
+
+vc.writeCurrentSimCfg = function() {
+    remote.getGlobal('simCfg').sNr = this._outlets.dropdown_sNr.val();
+    remote.getGlobal('simCfg').iavNr = this._outlets.dropdown_iavNr.val();
+    remote.getGlobal('simCfg').dNr = this._outlets.dropdown_dNr.val();
+}
+
+//Shows an element (tag) with the specified id (for a limited time))
+vc.showElement = function(elementId, tInSeconds = false) {
+    $(elementId).show();
+    if (tInSeconds) {
+        setTimeout(function() {
+            $(elementId).hide();
+        }, tInSeconds * 1000);
+    }
+}
+
+vc.hideElement = function(elementId) {
+    $(elementId).hide();
+}
+
+vc.addToConsole = function(message, color) {
     var currentDate = '[' + new Date().toLocaleString() + '] ';
     var pre = document.createElement("p");
     pre.innerHTML = currentDate + message;
     pre.style.color = color;
-    textviewConsole.appendChild(pre);
-}
-
-const buttonSendMessage = document.getElementById('uiButton_sendMessage');
-const textfieldMessage = document.getElementById('uiTextfield_message');
-
-textfieldMessage.addEventListener("keyup", function(event) {
-    // Number 13 is the "Enter" key on the keyboard
-    if (event.keyCode === 13) {
-        // Cancel the default action, if needed
-        event.preventDefault();
-        // Trigger the button element with a click
-        buttonSendMessage.click();
-    }
-});
-
-buttonSendMessage.addEventListener("click", function() {
-    message = textfieldMessage.value;
-    textfieldMessage.value = "";
-    ipcRenderer.send('asynchronous-message', message);
-})
-
-*/
-
-mx = {
-
+    this._outlets.console.append(pre);
 }
